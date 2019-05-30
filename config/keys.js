6af55a4390c9bf8bb2a4aae38ac5cb81decf2f86
Khoby-790 <ebaidoo79@gmail.com>
2019-05-30 23:04:55 +0800

    mood

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,11 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb://khoby:khoby1@ds263816.mlab.com:63816/resource_share';
 
 module.exports = {
     mongoURI: dbPassword
 };
+
+
+
+
+//
+//'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true
